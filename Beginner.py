a89fa07844d1a8a7af60d1c3770926fe6a45969a
UNILORN <yuoyun6427.yuniron.komoron@gmail.com>
2017-07-28 01:30:24 +0900

    ループ等

--- Beginner.py
+++ Beginner.py
@@ -12,4 +12,42 @@ print text
 text = str(4) + str(5)
 print text
 
+# 数値リスト
+list = [1, 5, 2, 4]
+print list
+
+# 文字列リスト
+list = ['A', 'B', 'C', 'D']
+print list
+
+# 混合リスト
+list = ['A', 'B', 3, 4]
+print list
+
+print list[1]
+print list[-1]
+print list[:2]
+print list[2:]
+print len(list)
+
+# リスト要素削除
+del list[1]
+print list
+
+list = list + ['B', 'C', 'D']
+print list
+
+# ループ
+for i in range(5):
+    print i
+
+# リストループ
+for i in list:
+    print i
+
+# インデックスリストループ
+for i in range(len(list)):
+    print i, list[i]
+
+# リスト内検索
 
