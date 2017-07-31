d0b538f2a2756ea1d0ff91c8e7c9b4721a9744c9
UNILORN <yuoyun6427.yuniron.komoron@gmail.com>
2017-07-31 19:02:47 +0900

    update

--- Sansho.py
+++ Sansho.py
@@ -1,2 +1,27 @@
 # coding=utf-8
 
+# 変数の参照
+
+list = [0, 3, 5, 7, 9]
+
+cheese = list
+
+print list
+print cheese
+
+list.append(10)
+
+print list
+print cheese
+
+# コピー
+
+import copy
+
+cheese = copy.copy(list)
+
+list.remove(10)
+
+print list
+print cheese
+
