d14078a60f6d8d2f1a3ffc32254fa129c7fcdf9f
UNILORN <yuoyun6427.yuniron.komoron@gmail.com>
2017-08-02 22:43:22 +0900

    文字列操作

--- Character.py
+++ Character.py
@@ -10,3 +10,27 @@ ccc
 '''
 print long_text
 print text
+
+# p133
+
+print text[0]
+print text[:5]
+
+print text.upper()
+print text.lower()
+print text[5:].islower()
+print text.isupper()
+
+# p138
+
+print text.startswith('Hello')
+print text.endswith('world .')
+
+print text.split()
+print ' '.join(text)
+
+# p140
+
+print text.rjust(20,'*')
+print text.ljust(20,'*')
+print text.center(20,'*')
