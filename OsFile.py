c55f5c8f602c123c59a86c58816ce0a1d447bca4
UNILORN <yuoyun6427.yuniron.komoron@gmail.com>
2017-08-02 22:57:28 +0900

    ファイル操作

--- OsFile.py
+++ OsFile.py
@@ -9,5 +9,24 @@ print os.path.getsize(path)
 print os.path.isfile(path)
 print os.path.isdir(path)
 
+# ファイル読み込み
 edit_file = open(path+'test.txt')
 print edit_file.read()
+edit_file.close()
+
+# ファイル単行読み込み
+edit_file = open(path+'test.txt')
+print edit_file.readline()
+edit_file.close()
+
+# ファイル書き込み
+edit_file = open(path+'test.txt','w')
+edit_file.write('Hello world\n')
+edit_file.close()
+
+# ファイル追記
+edit_file = open(path+'test.txt','a')
+edit_file.write('hogehoge\n')
+edit_file.write('piyopiyo\n')
+edit_file.close()
+
