c3ad8f33975d7686ce27ea3c4d5b0028f14155c1
UNILORN <yuoyun6427.yuniron.komoron@gmail.com>
2017-08-02 19:08:05 +0900

    辞書

--- Zisho.py
+++ Zisho.py
@@ -2,3 +2,20 @@
 
 # p109 辞書とデータ構造
 
+zisho = {'size': 'big', 'color': 'gray'}
+print zisho['size']
+
+# p110 比較
+
+spam = {'color': 'gray', 'size': 'big'}
+
+print spam == zisho
+
+# p112 メソッド
+
+print zisho
+
+print zisho.values()
+print zisho.keys()
+print zisho.items()
+
